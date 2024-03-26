d9338d8fafc0958464889fcb14e7605443db7625
restarthxr <165002879+restarthxr@users.noreply.github.com>
2024-03-26 17:39:09 +0800

    Add files via upload

---
+++ InpGCL/Utils/TimeLogger.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+
+logmsg = ''
+
+timemark = dict()
+
+saveDefault = False
+
+
+def log(msg, save=None, oneline=False):
+    global logmsg
+
+    global saveDefault
+
+    time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    tem = '%s: %s' % (time, msg)
+
+    if save != None:
+
+        if save:
+            logmsg += tem + '\n'
+
+
+    elif saveDefault:
+
+        logmsg += tem + '\n'
+
+    if oneline:
+
+        print(tem, end='\r')
+
+    else:
+
+        print(tem)
+
+
+def marktime(marker):
+    global timemark
+
+    timemark[marker] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+if __name__ == '__main__':
+    log('')
